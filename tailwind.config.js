539a8a3e8ffe6bd81db98bd9cea71430ef657fb3
Kasui92 <pattocchioluca@gmail.com>
2023-11-03 11:02:33 +0100

    chore: set new base font-family;

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
     content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
     darkMode: 'class',
     theme: {
+        fontFamily: {
+            sans: ['Source Sans Pro', 'sans-serif'],
+        },
         extend: {
             colors: {
                 background: '#011b1f',
