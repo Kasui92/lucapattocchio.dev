0a5a0c508582e253a3731581344e00905f8f9079
Kasui92 <pattocchioluca@gmail.com>
2023-11-05 13:11:17 +0100

    feat: add skills and skill icons;

---
+++ src/config/skills.ts
@@ -0,0 +1,72 @@
+interface SkillEntry {
+    label: string
+    icon: string
+}
+
+/**
+ * Skills
+ */
+const skills: Array<SkillEntry> = [
+    {
+        label: 'HTML',
+        icon: 'html',
+    },
+    {
+        label: 'CSS',
+        icon: 'css',
+    },
+    {
+        label: 'PHP',
+        icon: 'php',
+    },
+    {
+        label: 'MySQL',
+        icon: 'mysql',
+    },
+    {
+        label: 'Javascript',
+        icon: 'javascript',
+    },
+    {
+        label: 'Typescript',
+        icon: 'typescript',
+    },
+    {
+        label: 'Node.js',
+        icon: 'nodejs',
+    },
+    {
+        label: 'React',
+        icon: 'react',
+    },
+    {
+        label: 'Next.js',
+        icon: 'nextjs',
+    },
+    {
+        label: 'PostgreSQL',
+        icon: 'postgresql',
+    },
+    {
+        label: 'Bootstrap',
+        icon: 'bootstrap',
+    },
+    {
+        label: 'TailwindCSS',
+        icon: 'tailwindcss',
+    },
+    {
+        label: 'Git',
+        icon: 'git',
+    },
+    {
+        label: 'Docker',
+        icon: 'docker',
+    },
+    {
+        label: 'Bash',
+        icon: 'bash',
+    },
+]
+
+export default skills
