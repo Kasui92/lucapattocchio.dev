990f2d2fbe2f0a785aae4c4f3c1f7d98c82f359b
Luca Pattocchio <pattocchioluca@gmail.com>
2024-11-12 17:48:10 +0100

    fix: update import paths for App and global styles

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-import App from './App'
+import App from '@/App'
 
-import './index.css'
+import '@/assets/styles/global.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
