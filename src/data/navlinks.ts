acf4feb49bee317711e836f4210fbcbf867275d4
Luca Pattocchio <pattocchioluca@gmail.com>
2024-11-14 16:26:55 +0100

    upd: add gitlab;

---
+++ src/data/navlinks.ts
@@ -0,0 +1,20 @@
+export interface NavLinksEntry {
+    label: string
+    url: string
+}
+
+/**
+ * Social links
+ */
+const navLinks: Array<NavLinksEntry> = [
+    {
+        label: 'About',
+        url: 'about',
+    },
+    {
+        label: 'Projects',
+        url: 'projects',
+    },
+]
+
+export default navLinks
