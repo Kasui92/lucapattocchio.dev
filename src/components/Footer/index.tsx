6946eca1ef3971c9e39ca5147b580e48f5fb1204
Kasui92 <pattocchioluca@gmail.com>
2025-02-10 00:04:01 +0100

    fix: update footer to display the current year dynamically

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -5,10 +5,13 @@ import { ReactElement } from 'react'
  * @constructor
  */
 export const Footer = (): ReactElement => {
+    // Get the current year
+    const currentYear = new Date().getFullYear()
+
     return (
         <footer className="mb-6 mt-12">
             <div className="flex justify-center space-x-2 text-sm text-silver-500/80">
-                <span>© 2024 Luca Pattocchio.</span>
+                <span>© {currentYear} Luca Pattocchio.</span>
                 <span>All Rights Reserved.</span>
             </div>
         </footer>
