941c2890caaf292b3bc6eb32d9d11220fc45ed23
Luca Pattocchio <pattocchioluca@gmail.com>
2025-12-17 09:54:29 +0100

    Add preload for profile picture

--- src/components/ProfilePic.tsx
+++ src/components/ProfilePic.tsx
@@ -3,8 +3,8 @@ import { ReactElement } from 'react'
 import profilePic from '../assets/images/profile_pic.jpg'
 
 export const HeroProfilePic = (): ReactElement => (
-    <div className="transform rounded-full shadow-lg ring-[5px] ring-silver-500/10 transition-transform duration-300 hover:scale-105 hover:ring-silver-500/20">
-        <img className="h-16 w-16 rounded-full object-cover" src={profilePic} alt="Logo" />
+    <div className="ring-silver-500/10 hover:ring-silver-500/20 transform rounded-full shadow-lg ring-[5px] transition-transform duration-300 hover:scale-105">
+        <img className="h-16 w-16 rounded-full object-cover" src={profilePic} alt="Logo" fetchPriority="high" />
     </div>
 )
 
@@ -15,7 +15,7 @@ export const NavProfilePic = (): ReactElement => (
             className="transform rounded-full transition-transform duration-300 hover:scale-110"
             title="Go to homepage"
         >
-            <img className="h-10 w-10 rounded-full object-cover" src={profilePic} alt="Logo" />
+            <img className="h-10 w-10 rounded-full object-cover" src={profilePic} alt="Logo" fetchPriority="high" />
         </a>
     </div>
 )
