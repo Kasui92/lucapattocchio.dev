4a98cd79c05dc119e86905b41ffe9ffdc2264261
Luca Pattocchio <pattocchioluca@gmail.com>
2026-01-10 10:50:09 +0100

    Enhance Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,39 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import path from "path";
+import { fileURLToPath } from "url";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
+import { remarkAlert } from "remark-github-blockquote-alert";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://lucapattocchio.dev",
+  base: "/",
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+    build: {
+      sourcemap: false,
+      rollupOptions: {
+        output: {
+          sourcemap: false,
+        },
+      },
+    },
+  },
+  markdown: {
+    remarkPlugins: [remarkAlert],
+    rehypePlugins: [],
+    shikiConfig: {
+      theme: "github-dark",
+      wrap: true,
+    },
+  },
 });
