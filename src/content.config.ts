06cd52c68fe161e5b063ea202d35a2747797c484
Luca Pattocchio <pattocchioluca@gmail.com>
2026-01-29 12:15:05 +0100

    Add experiences and works collections to content configuration

--- src/content.config.ts
+++ src/content.config.ts
@@ -20,7 +20,59 @@ const navigation = defineCollection({
   }),
 });
 
+const experiences = defineCollection({
+  loader: file("src/data/experiences.json", {
+    parser: (data) => {
+      const arrayData = JSON.parse(data) as any[];
+      return arrayData?.map((item, index) => {
+        return {
+          id: index.toString(),
+          ...item,
+        };
+      });
+    },
+  }),
+  schema: z.object({
+    position: z.string(),
+    company: z.string(),
+    companyUrl: z.string().url(),
+    workExperience: z.string(),
+    startDate: z.string().refine((date) => !isNaN(Date.parse(date)), {
+      message: "Invalid date format",
+    }),
+    endDate: z
+      .string()
+      .refine((date) => !isNaN(Date.parse(date)), {
+        message: "Invalid date format",
+      })
+      .optional(),
+    location: z.string(),
+  }),
+});
+
+const works = defineCollection({
+  loader: file("src/data/works.json", {
+    parser: (data) => {
+      const arrayData = JSON.parse(data) as any[];
+      return arrayData?.map((item, index) => {
+        return {
+          id: index.toString(),
+          ...item,
+        };
+      });
+    },
+  }),
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    image: z.string(),
+    url: z.string().url(),
+  }),
+});
+
 export const collections = {
   socials,
   navigation,
+  experiences,
+  works,
 };
