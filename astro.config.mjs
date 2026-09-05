8812b341e3c7b4a577c28f67f48c37b14fb1cf43
Luca Pattocchio <15154851+Kasui92@users.noreply.github.com>
2026-09-05 23:24:46 +0200

    fix: ensure react dependency optimization

--- astro.config.mjs
+++ astro.config.mjs
@@ -23,6 +23,10 @@ export default defineConfig({
       alias: {
         "@": path.resolve(__dirname, "./src"),
       },
+      dedupe: ["react", "react-dom"],
+    },
+    optimizeDeps: {
+      include: ["react", "react-dom", "react-intersection-observer"],
     },
     build: {
       sourcemap: false,
